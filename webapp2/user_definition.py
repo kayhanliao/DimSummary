e69e47294493963988b6d02d4f82b29a78faf937
Chenxi Li <lichenxi1113@gmail.com>
2023-04-19 13:01:14 -0700

    chenxi version search bar updates

---
+++ webapp2/user_definition.py
@@ -0,0 +1,2 @@
+url = 'https://api.yelp.com/v3/businesses/search'
+api_key = 'Bearer CrgilvjOJW-U-DnMiyFz_xypRqtVtfL_yekfgb4Kg5xNRWe1A83s2llrJ7RxnXYa0iqT8GwTCHJZKvYMHL7CmV1zJPe0D1_jWrH8W0bMxTtR6Ufksbg5REDVrSDbY3Yx'
